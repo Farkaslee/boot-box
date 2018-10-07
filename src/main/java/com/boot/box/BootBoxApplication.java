ce25b7af6dd4004098c3cb4fb561685838855527
farkaslee <lfl969605@126.com>
2018-10-07 22:02:26 +0800

    add filter

--- src/main/java/com/boot/box/BootBoxApplication.java
+++ src/main/java/com/boot/box/BootBoxApplication.java
@@ -3,9 +3,11 @@ package com.boot.box;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@ServletComponentScan
 @PropertySource(value="classpath:my.properties",encoding="utf-8")
 @Slf4j
 public class BootBoxApplication {
